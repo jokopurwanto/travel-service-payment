272f675b5e1fc08b726e88b0b5a5de058ebf01f3
jokopurwanto <jokopurwanto1996@yahoo.com>
2023-05-04 11:03:53 +0700

    update timeout resttemplate

--- src/main/java/com/travel/payment/PaymentApplication.java
+++ src/main/java/com/travel/payment/PaymentApplication.java
@@ -17,8 +17,8 @@ public class PaymentApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder){
 		return builder
-				.setConnectTimeout(Duration.ofMillis(3000))
-				.setReadTimeout(Duration.ofMillis(3000))
+				.setConnectTimeout(Duration.ofMillis(10000))
+				.setReadTimeout(Duration.ofMillis(10000))
 				.build();
 	}
 
